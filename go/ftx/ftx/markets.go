8da0d0a77b5e507b0fa9417e0558ba9eda66870b
dataBaseError <heron.joseph@gmail.com>
2021-12-06 18:43:31 -0500

    Add ftx endpoints

--- go/ftx/ftx/markets.go
+++ go/ftx/ftx/markets.go
@@ -57,3 +57,89 @@ func (client *FtxClient) GetOrderbook(market string, depth int64) (structs.Order
 	err = _processResponse(resp, &orderbook)
 	return orderbook, resp, err
 }
+
+func (client *FtxClient) Markets() (structs.MarketsResponse, *http.Response, error) {
+	var markets_resp structs.MarketsResponse
+	resp, err := client._get("markets", []byte(""))
+	if err != nil {
+		// log.Printf("Error GetTrades", err)
+		return markets_resp, resp, err
+	}
+	err = _processResponse(resp, &markets_resp)
+	return markets_resp, resp, err
+}
+
+func (client *FtxClient) Futures() (structs.FuturesResponse, *http.Response, error) {
+	var future_resp structs.FuturesResponse
+	resp, err := client._get("futures", []byte(""))
+	if err != nil {
+		// log.Printf("Error GetTrades", err)
+		return future_resp, resp, err
+	}
+	err = _processResponse(resp, &future_resp)
+	return future_resp, resp, err
+}
+
+func (client *FtxClient) FutureStat(future string) (structs.FutureStatsResponse, *http.Response, error) {
+	var future_resp structs.FutureStatsResponse
+	resp, err := client._get("futures/"+future, []byte(""))
+	if err != nil {
+		// log.Printf("Error GetTrades", err)
+		return future_resp, resp, err
+	}
+	err = _processResponse(resp, &future_resp)
+	return future_resp, resp, err
+}
+
+func (client *FtxClient) LendingRates() (structs.LendingRatesResponse, *http.Response, error) {
+	var lendingRates structs.LendingRatesResponse
+	resp, err := client._get("spot_margin/lending_rates", []byte(""))
+	if err != nil {
+		// log.Printf("Error GetTrades", err)
+		return lendingRates, resp, err
+	}
+	err = _processResponse(resp, &lendingRates)
+	return lendingRates, resp, err
+}
+
+func (client *FtxClient) LendingHistory(startTime null.Int, endTime null.Int) (structs.LendingHistoryResponse, *http.Response, error) {
+	var lendingRateHistory structs.LendingHistoryResponse
+	params := ""
+	if !startTime.IsZero() {
+		params = "?start_time=" + strconv.FormatInt(startTime.ValueOrZero(), 10)
+	}
+	if !endTime.IsZero() {
+		if len(params) == 0 {
+			params = "?"
+		} else {
+			params += "&"
+		}
+		params += "end_time=" + strconv.FormatInt(endTime.ValueOrZero(), 10)
+	}
+	resp, err := client._get("spot_margin/history"+params, []byte(""))
+	if err != nil {
+		// log.Printf("Error GetTrades", err)
+		return lendingRateHistory, resp, err
+	}
+	err = _processResponse(resp, &lendingRateHistory)
+	return lendingRateHistory, resp, err
+}
+
+func (client *FtxClient) HistoricalPrices(market string, resolution int64, startTime null.Int, endTime null.Int) (structs.HistoricalPricesResponse, *http.Response, error) {
+	var historyPrice structs.HistoricalPricesResponse
+	params := ""
+	if !startTime.IsZero() {
+		params = "&start_time=" + strconv.FormatInt(startTime.ValueOrZero(), 10)
+	}
+	if !endTime.IsZero() {
+		params += "&end_time=" + strconv.FormatInt(endTime.ValueOrZero(), 10)
+	}
+	resp, err := client._get("markets/"+market+"/candles?resolution="+
+		strconv.FormatInt(resolution, 10)+params, []byte(""))
+	if err != nil {
+		// log.Printf("Error GetTrades", err)
+		return historyPrice, resp, err
+	}
+	err = _processResponse(resp, &historyPrice)
+	return historyPrice, resp, err
+}
--- go/ftx/ftx/structs/markets.go
+++ go/ftx/ftx/structs/markets.go
@@ -35,3 +35,127 @@ type Orderbook struct {
 		Bids [][]float64 `json:"bids"`
 	} `json:"result"`
 }
+
+type MarketsResponse struct {
+	Success bool `json:"success"`
+	Result  []struct {
+		Name                  string      `json:"name"`
+		Enabled               bool        `json:"enabled"`
+		PostOnly              bool        `json:"postOnly"`
+		PriceIncrement        float64     `json:"priceIncrement"`
+		SizeIncrement         float64     `json:"sizeIncrement"`
+		MinProvideSize        float64     `json:"minProvideSize"`
+		Last                  float64     `json:"last"`
+		Bid                   float64     `json:"bid"`
+		Ask                   float64     `json:"ask"`
+		Price                 float64     `json:"price"`
+		Type                  string      `json:"type"`
+		BaseCurrency          interface{} `json:"baseCurrency"`
+		QuoteCurrency         interface{} `json:"quoteCurrency"`
+		Underlying            string      `json:"underlying"`
+		Restricted            bool        `json:"restricted"`
+		HighLeverageFeeExempt bool        `json:"highLeverageFeeExempt"`
+		Change1H              float64     `json:"change1h"`
+		Change24H             float64     `json:"change24h"`
+		ChangeBod             float64     `json:"changeBod"`
+		QuoteVolume24H        float64     `json:"quoteVolume24h"`
+		VolumeUsd24H          float64     `json:"volumeUsd24h"`
+		TokenizedEquity       bool        `json:"tokenizedEquity,omitempty"`
+	} `json:"result"`
+}
+
+type FuturesResponse struct {
+	Success bool `json:"success"`
+	Result  []struct {
+		Name                  string      `json:"name"`
+		Underlying            string      `json:"underlying"`
+		Description           string      `json:"description"`
+		Type                  string      `json:"type"`
+		Expiry                interface{} `json:"expiry"`
+		Perpetual             bool        `json:"perpetual"`
+		Expired               bool        `json:"expired"`
+		Enabled               bool        `json:"enabled"`
+		PostOnly              bool        `json:"postOnly"`
+		PriceIncrement        float64     `json:"priceIncrement"`
+		SizeIncrement         float64     `json:"sizeIncrement"`
+		Last                  float64     `json:"last"`
+		Bid                   float64     `json:"bid"`
+		Ask                   float64     `json:"ask"`
+		Index                 float64     `json:"index"`
+		Mark                  float64     `json:"mark"`
+		ImfFactor             float64     `json:"imfFactor"`
+		LowerBound            float64     `json:"lowerBound"`
+		UpperBound            float64     `json:"upperBound"`
+		UnderlyingDescription string      `json:"underlyingDescription"`
+		ExpiryDescription     string      `json:"expiryDescription"`
+		MoveStart             interface{} `json:"moveStart"`
+		MarginPrice           float64     `json:"marginPrice"`
+		PositionLimitWeight   float64     `json:"positionLimitWeight"`
+		Group                 string      `json:"group"`
+		Change1H              float64     `json:"change1h"`
+		Change24H             float64     `json:"change24h"`
+		ChangeBod             float64     `json:"changeBod"`
+		VolumeUsd24H          float64     `json:"volumeUsd24h"`
+		Volume                float64     `json:"volume"`
+		OpenInterest          float64     `json:"openInterest"`
+		OpenInterestUsd       float64     `json:"openInterestUsd"`
+	} `json:"result"`
+}
+
+type LendingRatesResponse struct {
+	Success bool `json:"success"`
+	Result  []struct {
+		Coin     string  `json:"coin"`
+		Previous float64 `json:"previous"`
+		Estimate float64 `json:"estimate"`
+	} `json:"result"`
+}
+
+type FutureStatsResponse struct {
+	Success bool `json:"success"`
+	Result  struct {
+		Ask            int       `json:"ask"`
+		Bid            float64   `json:"bid"`
+		Change1H       int       `json:"change1h"`
+		Change24H      int       `json:"change24h"`
+		Description    string    `json:"description"`
+		Enabled        bool      `json:"enabled"`
+		Expired        bool      `json:"expired"`
+		Expiry         time.Time `json:"expiry"`
+		Index          float64   `json:"index"`
+		Last           int       `json:"last"`
+		LowerBound     float64   `json:"lowerBound"`
+		Mark           float64   `json:"mark"`
+		Name           string    `json:"name"`
+		Perpetual      bool      `json:"perpetual"`
+		PostOnly       bool      `json:"postOnly"`
+		PriceIncrement float64   `json:"priceIncrement"`
+		SizeIncrement  float64   `json:"sizeIncrement"`
+		Underlying     string    `json:"underlying"`
+		UpperBound     float64   `json:"upperBound"`
+		Type           string    `json:"type"`
+	} `json:"result"`
+}
+
+type LendingHistoryResponse struct {
+	Success bool `json:"success"`
+	Result  []struct {
+		Coin string    `json:"coin"`
+		Time time.Time `json:"time"`
+		Size float64   `json:"size"`
+		Rate float64   `json:"rate"`
+	} `json:"result"`
+}
+
+type HistoricalPricesResponse struct {
+	Success bool `json:"success"`
+	Result  []struct {
+		StartTime time.Time `json:"startTime"`
+		Time      float64   `json:"time"`
+		Open      float64   `json:"open"`
+		High      float64   `json:"high"`
+		Low       float64   `json:"low"`
+		Close     float64   `json:"close"`
+		Volume    float64   `json:"volume"`
+	} `json:"result"`
+}
